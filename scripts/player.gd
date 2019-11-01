b62ca0f55093829388345a91f34dc8a340fdcd64
CLAUDIO JORGE LOPES FILHO <claudio.jorge@D81918.unifesp.br>
2019-11-01 09:52:19 -0300

    camera snap

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,9 +11,18 @@ const DASH_SPEED = 1000
 var movedir = 0
 var velocity = Vector2(0,0)
 
+onready var cam = $Camera2D
+onready var camHandler = $CameraHandler
+onready var camTween = $CameraHandler/CameraTween
+
+func _ready():
+	camHandler.connect("area_entered", self, "new_camera_snap")
+
 func _physics_process(delta):
+	if camTween.is_active():
+		return
 	
-	#                                      ANIMAÇÕES
+	# ANIMAÇÕES
 	if(Input.is_action_pressed("ui_right")):
 		if($AnimatedSprite.flip_h == true):
 			$AnimatedSprite.offset.x = 15
@@ -34,10 +43,9 @@ func _physics_process(delta):
 			$AnimatedSprite.play("Parado")
 		
 	
-	#                                     AÇÕES
+	# AÇÕES
 	
 	movedir = -int(Input.is_action_pressed("ui_left")) + int(Input.is_action_pressed("ui_right"))
-	
 	
 	velocity.x = movedir * SPEED
 	velocity.y += GRAVITY
@@ -55,5 +63,18 @@ func _physics_process(delta):
 func _on_Timer_timeout():
 	SPEED = 200
 
+func new_camera_snap(snap):
+	if snap.is_in_group("camera_snap"):
+		var camRect = Rect2(snap.position, Vector2(1024, 600) * snap.scale)
+		#cam.limit_left = snap.position.x
+		#cam.limit_right = snap.position.x + 1024 * snap.scale.x
+		#cam.limit_top = snap.position.y
+		#cam.limit_bottom = snap.position.y + 600 * snap.scale.y
+		tween_camera(camRect)
 
-
+func tween_camera(new_position):
+	camTween.interpolate_property(cam, "limit_left", cam.limit_left, new_position.position.x, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	camTween.interpolate_property(cam, "limit_right", cam.limit_right, new_position.end.x, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	camTween.interpolate_property(cam, "limit_top", cam.limit_top, new_position.position.y, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	camTween.interpolate_property(cam, "limit_bottom", cam.limit_bottom, new_position.end.y, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	camTween.start()
