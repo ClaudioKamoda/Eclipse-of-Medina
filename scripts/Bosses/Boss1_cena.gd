685d01441fe8642c48a810a8ec656ab62ec14fb7
Claudio <claudiojlf@gmail.com>
2019-12-06 01:35:03 -0300

    espinho adicionado

--- scripts/Bosses/Boss1_cena.gd
+++ scripts/Bosses/Boss1_cena.gd
@@ -4,6 +4,6 @@ onready var camera = $player/Camera2D
 
 func _ready():
 	camera.limit_top = 0
-	camera.limit_left = 0
+	camera.limit_left = -20
 	camera.limit_bottom = 600
 	camera.limit_right = 1728
